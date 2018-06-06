76f7a952cd6210df5d0d6b8dcbb87bc468113d3a
APA42 <alberto.p.42@gmail.com>
2018-06-06 20:24:04 +0200

    Iteration 1. Added test to add the crafter if all validations are successfully

--- crafterbook/specs/actions/add_crafter_spec.py
+++ crafterbook/specs/actions/add_crafter_spec.py
@@ -9,8 +9,9 @@ from doublex_expects import have_been_called_with
 
 class AddCrafter(object):
 
-    def __init__(self, validator):
+    def __init__(self, validator, crafter_management_service):
         self._validator = validator
+        self._crafter_management_service = crafter_management_service
 
     def execute(self, input_data):
         crafter_name_key = 'crafter_name'
@@ -21,12 +22,14 @@ class AddCrafter(object):
 
         self._validator.raise_error_if_crafter_name_exists(input_data[crafter_name_key])
 
+        self._crafter_management_service.add(input_data)
 
 
 with description('Add Crafter Action specs') as self:
     with before.each:
         self.validator = Spy()
-        self.add_crafter_action = AddCrafter(self.validator)
+        self.crafter_management_service = Spy()
+        self.add_crafter_action = AddCrafter(self.validator, self.crafter_management_service)
 
     with context('when adding a crafter'):
         with context('runs validations for mandatory parameters'):
@@ -62,3 +65,12 @@ with description('Add Crafter Action specs') as self:
                     self.add_crafter_action.execute(a_json_input_data)
 
                     expect(self.validator.raise_error_if_crafter_name_exists).to(have_been_called_with(a_crafter_name))
+
+        with context('when all validations are successful'):
+            with it('calls to crafter_management_service to add the crafter'):
+                a_crafter_name = 'a_crafter_name'
+                a_json_input_data = {'crafter_name': a_crafter_name}
+
+                self.add_crafter_action.execute(a_json_input_data)
+
+                expect(self.crafter_management_service.add).to(have_been_called_with(a_json_input_data))
